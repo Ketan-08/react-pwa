65b811509919afcd968a0f29cc01e5cc8534b96d
Ketan-08 <78249619+Ketan-08@users.noreply.github.com>
2022-05-01 12:11:58 +0530

    Updates

---
+++ precache-manifest.eba38248734e1c3fffc7b8ddfb9304af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc675a7d8fbdf42c4a42297e091dc62",
+    "url": "/react-pwa/index.html"
+  },
+  {
+    "revision": "392bdbb9ef7bae9651bb",
+    "url": "/react-pwa/static/css/main.0527dc8c.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/react-pwa/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pwa/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392bdbb9ef7bae9651bb",
+    "url": "/react-pwa/static/js/main.90d1f581.chunk.js"
+  },
+  {
+    "revision": "fbd288e4cc1b06c9ddaf",
+    "url": "/react-pwa/static/js/runtime-main.69c81f04.js"
+  }
+]);
